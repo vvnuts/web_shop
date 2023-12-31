d34cd1382758cdb7ae97699c47e0dcff1302c309
vvnuts <d.samoylov@nicetu.spb.ru>
2023-12-31 23:37:14 +0300

    Сделал фильтр по характеристикам продукта

--- shop/src/main/java/com/vvnuts/shop/services/specifications/ItemSpecification.java
+++ shop/src/main/java/com/vvnuts/shop/services/specifications/ItemSpecification.java
@@ -3,20 +3,42 @@ package com.vvnuts.shop.services.specifications;
 
 import com.vvnuts.shop.dtos.requests.SpecificationItemRequest;
 import com.vvnuts.shop.entities.Category;
+import com.vvnuts.shop.entities.CharacterItem;
+import com.vvnuts.shop.entities.Characteristic;
 import com.vvnuts.shop.entities.Item;
+import com.vvnuts.shop.entities.enums.Type;
+import com.vvnuts.shop.repositories.CharacteristicRepository;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.Predicate;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 @Service
+@RequiredArgsConstructor
 public class ItemSpecification {
+    private final CharacteristicRepository characteristicRepository;
+
     public Specification<Item> createSpecification(SpecificationItemRequest request) {
         Specification<Item> itemSpecification = getItemInStock();
 
         if (request.getRangeOfPrice() != null) {
-            itemSpecification = itemSpecification.and(getItemWithRangeOfMark(request.getRangeOfPrice()));
+            itemSpecification = itemSpecification.and(getItemWithRangeOfPrice(request.getRangeOfPrice()));
+        }
+        if (request.getRangeOfMark() != null) {
+            itemSpecification = itemSpecification.and(getItemWithRangeOfMark(request.getRangeOfMark()));
+        }
+        if (request.getCharacteristicValue() != null) {
+            for (Map.Entry<Integer, String> pair : request.getCharacteristicValue().entrySet()) {
+                Characteristic characteristic = characteristicRepository.findById(pair.getKey()).orElseThrow();
+                itemSpecification = itemSpecification.and(getItemWithCharacteristic(characteristic, pair.getValue()));
+            }
         }
 
         return itemSpecification;
@@ -32,13 +54,48 @@ public class ItemSpecification {
                 criteriaBuilder.equal(root.get("category"), id);
     }
 
-    private Specification<Item> getItemWithRangeOfMark(String range) {
-        String[] stingLimits = range.split("-");
-        List<Integer> limits = Arrays.stream(stingLimits).map(Integer::parseInt).toList();
+    private Specification<Item> getItemWithRangeOfPrice(String range) {
+        String[] stringLimits = range.split("-");
+        List<Integer> limits = Arrays.stream(stringLimits).map(Integer::parseInt).toList();
         if (limits.get(0) > limits.get(1)) {
-            return null;
+            return null; //TODO exception
         }
         return (root, query, criteriaBuilder) ->
                 criteriaBuilder.between(root.get("price"), limits.get(0), limits.get(1));
+    }
+
+    private Specification<Item> getItemWithRangeOfMark(String range) {
+        List<Double> limits = prepareStringLimits(range);
+        return (root, query, criteriaBuilder) ->
+                criteriaBuilder.between(root.get("mark"), limits.get(0), limits.get(1));
+    }
+
+    private Specification<Item> getItemWithCharacteristic(Characteristic characteristic, String value) {
+        return (root, query, criteriaBuilder) -> {
+            Join<Item, CharacterItem> itemWithCharacterItem = root.join("characterItems");
+            List<Predicate> predicates = new ArrayList<>();
+            predicates.add(criteriaBuilder.equal(itemWithCharacterItem.get("characteristic"), characteristic));
+            Join<CharacterItem, Characteristic> allInfo = itemWithCharacterItem.join("characteristic");
+
+            if (value.matches("\\d\\.\\d-\\d\\.\\d") && value.split("-").length == 2) {
+                List<Double> limits = prepareStringLimits(value);
+                predicates.add(criteriaBuilder.equal(allInfo.get("type"), Type.INTEGER.toString()));
+                predicates.add(criteriaBuilder.between(allInfo.get("numValue"), limits.get(0), limits.get(1)));
+            } else {
+                predicates.add(criteriaBuilder.equal(allInfo.get("type"), Type.STRING.toString()));
+                predicates.add(criteriaBuilder.equal(allInfo.get("value"), value));
+            }
+
+            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+        };
+    }
+
+    private List<Double> prepareStringLimits(String value) {
+        String[] stringLimits = value.split("-");
+        List<Double> limits = Arrays.stream(stringLimits).map(Double::parseDouble).toList();
+        if (limits.get(0) > limits.get(1) && limits.get(0) >= 0 && limits.get(1) <= 5) {
+            throw new RuntimeException(); //TODO exception
+        }
+        return limits;
     }
 }
